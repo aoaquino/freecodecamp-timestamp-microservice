3d4a2ba5e9f330c4f9c15fece1c2593dbaeb88fe
Glitch (freecodecamp-timestamp-aoaquino) <none>
2022-04-23 11:00:21 +0000

    🪠😷 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,80 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+// Timestamp Microservice
+let responseObject = {}
+
+app.get("/api/:input", function (request, response) {
+  let input = request.params.input
+  
+  
+  
+  if (/\d{5,}/.test(input)) {
+
+    if(input.includes('-')){
+      responseObject['unix'] = new Date(input).getTime()
+      responseObject['utc'] = new Date(input).toUTCString()
+    }
+    else{
+      input = parseInt(input)
+
+      responseObject['unix'] = new Date(input).getTime()
+      responseObject['utc'] = new Date(input).toUTCString()
+    }
+  } else {
+
+    let dateObject = new Date(input);
+
+    if (dateObject.toString() === "Invalid Date") {
+
+      response.json({ error: "Invalid Date" });
+
+    } else {
+      responseObject['unix'] = dateObject.valueOf()
+      responseObject['utc'] = dateObject.toUTCString()
+      
+      response.json(responseObject);
+
+    }
+  }
+  
+  response.json(responseObject);
+});
+
+app.get("/api/", function (req, res) {
+  responseObject['unix'] = new Date().getTime()
+  responseObject['utc'] = new Date().toUTCString()
+  
+  res.json(responseObject);
+});
+
